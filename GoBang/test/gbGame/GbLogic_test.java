e4caee3928b3660a637c935c0e6b7a55d7ce71c8
habuehri <habuehri@htwg-konstanz.de>
2014-11-09 13:55:47 +0100

    Gamefield Logic geschrieben und Testklasse

--- GoBang/test/gbGame/GbLogic_test.java
+++ GoBang/test/gbGame/GbLogic_test.java
@@ -1,5 +1,24 @@
 package gbGame;
 
+import gbData.gameToken;
+import gbData.tokenO;
+import gbData.tokenX;
+
+
 public class GbLogic_test {
 
+	public static void main(String[] args) {
+		gameToken Player1 = new tokenX();
+		gameToken Player2 = new tokenO();
+		GbLogic myGame = new GbLogic(Player1, Player2);
+		System.out.println(myGame.getCounter());
+		System.out.println(myGame.getcPlayer().getName());
+		myGame.setToken(5, 5);
+		System.out.println(myGame.getcPlayer().getName());
+		myGame.setToken(5, 6);
+		System.out.println(myGame.getcPlayer().getName());
+		myGame.setToken(5, 6);
+		System.out.println("Spieler " + myGame.getcPlayer().getName() + " ist wieder dran.");
+		System.out.println(myGame.getCounter());
+	}
 }
