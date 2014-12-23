8eff7181e5d9f3cdc8f3d4c7fe411432e083b8ae
niutzelm <niutzelm@htwg-konstanz.de>
2014-12-23 10:20:14 +0100

    started with GUI

---
+++ GoBang/src/de/htwg/gobang/ui/GUI.java
@@ -0,0 +1,22 @@
+package de.htwg.gobang.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI extends JFrame implements ActionListener{
+
+	//TopPanel
+	
+	//MainPanel
+	JPanel mainPanel = new JPanel();
+	mainPanel.setLayout(new BorderLayout());
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
