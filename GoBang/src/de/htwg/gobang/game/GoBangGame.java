addcf61b69a2633aefe764816ee2b3ac195d17f4
habuehri <habuehri@htwg-konstanz.de>
2015-01-23 02:18:56 +0100

    first steps for observer pattern

---
+++ GoBang/src/de/htwg/gobang/game/GoBangGame.java
@@ -0,0 +1,22 @@
+package de.htwg.gobang.game;
+import java.util.Observable;
+
+import de.htwg.gobang.controller.GbLogic;
+import de.htwg.gobang.entities.TokenBlack;
+import de.htwg.gobang.entities.TokenWhite;
+import de.htwg.gobang.observer.MyObserverable;
+import de.htwg.gobang.ui.GUI;
+import de.htwg.gobang.ui.TUI;
+
+public class GoBangGame extends Observable{
+
+	public static void main(String[] args) {
+			
+		GbLogic myController = new GbLogic(new TokenBlack(), new TokenWhite());
+		MyObserverable obs = new MyObserverable();
+		GUI myGUI = new GUI(myController);
+		obs.addObserver(myGUI);
+		TUI myTUI = new TUI(myController);
+		obs.addObserver(myTUI);
+	}
+}
